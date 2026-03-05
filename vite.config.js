aa975c298cd572001e2b6797939d1ab64320cb1b
Keval Bharda <kevalbharda31@gmail.com>
2026-03-05 18:58:48 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Add your host here
+    allowedHosts: ['devserver-main--feedbacka.netlify.app']
+  }
 })
